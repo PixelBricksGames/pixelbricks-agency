5517fdc8cdb5f3563b80b5e9a51bb9f908a975be
Pabrick <pabrick.bastaky@gmail.com>
2019-07-13 18:46:58 +0100

    +ADD: state

--- src/components/App.js
+++ src/components/App.js
@@ -8,22 +8,26 @@ import Alert from "./alert/Alert";
 import "./App.scss";
 
 class App extends Component {
-    render() {
-        const status = {
+
+    state = {
+        status: {
             date: new Date(),
             reputation: -35,
-            happines: 12.5,
+            karma: 10,
+            happiness: 12.5,
             quality: 34.5,
             money: 125000000
-        };
-        const workCompleted = 2;
-        const alertMessage = "ola ke ase, ke dise amigo, te vienes a por unas mistas ricas? A ver si tengo ñ y Ñ, no vaya a sér que luego ná.";
-        console.log(workCompleted);
+        },
+        workCompleted: 2,
+        alertMessage: "ola ke ase, ke dise amigo, te vienes a por unas mistas ricas? A ver si tengo ñ y Ñ, no vaya a sér que luego ná.",
+    }
+
+    render() {
         return (
             <div className="app">
-                <Alert message={alertMessage} />
-                <Menu notifications={workCompleted} />
-                <Status {...status} />
+                <Alert message={this.state.alertMessage} />
+                <Menu notifications={this.state.workCompleted} />
+                <Status {...this.state.status} />
                 <Office />
             </div>
         );
--- src/components/status/Status.js
+++ src/components/status/Status.js
@@ -7,18 +7,19 @@ import InfoBar from "./info-bar/InfoBar";
 import "./Status.scss";
 
 const Status = (props) => {
+
     return (
         <section className="status">
             <RoundCalendar date={props.date}/>
             <div className="status__labels">
-                <div className="status__labels--icon_karma-green"></div>
-                <div className="status__labels--icon_emoji-happy"></div>
+                <div className={`status__labels--icon_karma-${props.reputation < 0 ? "red" : "green"}`}></div>
+                <div className={`status__labels--icon_emoji-${props.happiness > 50 ? "happy" : "angry"}`}></div>
                 <div className="status__labels--icon-folder"></div>
                 <div className="status__labels--icon-currency"></div>
             </div>
             <div className="status__info">
                 <InfoBar value={props.reputation}/>
-                <InfoBar value={props.happines} percentage={true} color="#FFCC33"/>
+                <InfoBar value={props.happiness} percentage={true} color="#FFCC33"/>
                 <InfoBar value={props.quality} percentage={true} color="#9DCE90" />
                 <InfoBar value={props.money} />
             </div>
@@ -29,7 +30,7 @@ const Status = (props) => {
 Status.propTypes = {
     date: PropTypes.instanceOf(Date).isRequired,
     reputation: PropTypes.number.isRequired,
-    happines: PropTypes.number.isRequired,
+    happiness: PropTypes.number.isRequired,
     quality: PropTypes.number.isRequired,
     money: PropTypes.number.isRequired
 };
